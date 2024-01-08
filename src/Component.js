a76ba6c5812acba2dcd78d699f5058bd226f94ac
marcocareddu <saldumaccu@tuta.io>
2024-01-08 11:43:44 +0100

    Created Component.js

---
+++ src/Component.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Component = () => {
+  return (
+    <div>
+      <h2>Questo è il componente chiamato "Component.js"</h2>
+    </div>
+  )
+}
+
+export default Component
