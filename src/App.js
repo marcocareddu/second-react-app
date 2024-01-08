d52ffb50c10853c4def3568e02155bafbaa32b4a
marcocareddu <saldumaccu@tuta.io>
2024-01-08 17:17:51 +0100

    Added props

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import Component from "./Component";
 import Card from "./Card";
 
+const card1 = {
+  username: "Marco",
+  price: 2.50,
+  url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b13305_1.jpg",
+  title: "200€ Airbnb +400P"
+}
+
+const card2 = {
+  username: "Lucia",
+  price: 5.50,
+  url: "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b17471_1.jpg",
+  title: "20€ PS Store +40P"
+}
+
 function App() {
   return (
     <div className="App">
       <h1>Ciao a tutti</h1>
       <Component />
       <div className="d-flex justify-content-evenly row">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+
+        <Card 
+        username = { card1.username}
+        price = { card1.price}
+        url = { card1.url}
+        title = { card1.title}
+        />
+        <Card 
+        username = { card2.username}
+        price = { card2.price}
+        url = { card2.url}
+        title = { card2.title}
+        />
+
       </div>
     </div>
   );
--- src/Card.js
+++ src/Card.js
@@ -1,19 +1,14 @@
 import React from "react";
-const img =
-  "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b13305_1.jpg";
 
-const price = 4.26
-const name = "Marco"
-
-const Card = () => {
+const Card = (props) => {
   return (
     <article className="col-3 d-flex flex-column justify-content-center align-items-center">
-      <img src={img} alt="immagine di card" className="img-fluid" />
+      <img src={props.url} alt="immagine di card" className="img-fluid" />
       <div className="text-center w-100">
-        <h6>200€ Airbnb +400P</h6>
+        <h6>{props.title}</h6>
         <div className="divisore my-3"></div>
-        <h5 className="price fw-bold">{price}</h5>
-        <h6>{name}</h6>
+        <h5 className="price fw-bold">{props.price}</h5>
+        <h6>{props.username}</h6>
         <p className="timer fw-bold">00:05</p>
         <div className="puntata">PUNTA</div>
       </div>
