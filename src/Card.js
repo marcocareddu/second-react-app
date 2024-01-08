2ac1c3f13fe87aecb20f5cc2896bedc5134435b1
marcocareddu <saldumaccu@tuta.io>
2024-01-08 15:34:41 +0100

    Added img

--- src/Card.js
+++ src/Card.js
@@ -1,8 +1,11 @@
 import React from 'react'
+const img = "https://a71eba0458acf57331d3-d31ce5ebd093935dff8526660841b743.ssl.cf2.rackcdn.com/products/b13305_1.jpg"
 
 const Card = () => {
     return (
-        <h1>ciccio</h1>
+        <article>
+            <img src={img} alt="immagine di card" />
+        </article>
     )
 }
 
