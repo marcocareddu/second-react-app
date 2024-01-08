bc5619bd42e251ff32522529d04418fc302fb62e
marcocareddu <saldumaccu@tuta.io>
2024-01-08 15:24:54 +0100

    Create and import card

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Component from "./Component";
+import Card from "./Card";
 
 function App() {
   return (
     <div className="App">
      <h1>Ciao a tutti</h1>
      <Component />
+     <Card />
     </div>
   );
 }
